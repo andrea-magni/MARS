e126e48f7323051777e7afede2d10a52f300d1d5
Andrea Magni <andrea.magni@gmail.com>
2016-10-23 10:42:07 +0200

    * cleanup (separated MARSServer demo from DelphiRazor demo)

---
+++ Demos/DelphiRazor/Server.Forms.Main.pas
@@ -0,0 +1,116 @@
+(*
+  Copyright 2016, MARS-Curiosity library
+
+  Home: https://github.com/andrea-magni/MARS
+
+
+
+*)
+unit Server.Forms.Main;
+
+interface
+
+uses Classes, SysUtils, Forms, ActnList, ComCtrls, StdCtrls, Controls, ExtCtrls
+  , Diagnostics
+
+  , MARS.Core.Engine
+  , MARS.http.Server.Indy
+
+  , MARS.Core.Application
+  , System.Actions
+  ;
+
+type
+  TMainForm = class(TForm)
+    TopPanel: TPanel;
+    StartButton: TButton;
+    StopButton: TButton;
+    MainActionList: TActionList;
+    StartServerAction: TAction;
+    StopServerAction: TAction;
+    PortNumberEdit: TEdit;
+    Label1: TLabel;
+    procedure StartServerActionExecute(Sender: TObject);
+    procedure StartServerActionUpdate(Sender: TObject);
+    procedure StopServerActionExecute(Sender: TObject);
+    procedure StopServerActionUpdate(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    FServer: TMARShttpServerIndy;
+    FEngine: TMARSEngine;
+  public
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.dfm}
+
+uses
+    MARS.Core.JSON
+  , MARS.Rtti.Utils
+  , MARS.Utils.Parameters.IniFile
+  , MARS.Core.URL
+  , MARS.Core.MessageBodyWriter
+  , MARS.Core.MessageBodyWriters
+  , Server.Resources
+  ;
+
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  StartServerAction.Execute;
+end;
+
+procedure TMainForm.StartServerActionExecute(Sender: TObject);
+begin
+  // MARS-Curiosity Engine
+  FEngine := TMARSEngine.Create;
+  try
+    FEngine.Parameters.LoadFromIniFile;
+    FEngine.AddApplication('Default', '/default', ['Server.Resources.*']);
+    FEngine.OnBeforeHandleRequest :=
+      function (AEngine: TMARSEngine; AURL: TMARSURL): Boolean
+      begin
+        Result := True;
+        if AURL.Resource.EndsWith('favicon.ico', true) then
+          Result := False;
+      end;
+
+    // http server implementation
+    FServer := TMARShttpServerIndy.Create(FEngine);
+    try
+      FServer.Active := True;
+    except
+      FServer.Free;
+      raise;
+    end;
+  except
+    FEngine.Free;
+    raise;
+  end;
+end;
+
+procedure TMainForm.StartServerActionUpdate(Sender: TObject);
+begin
+  StartServerAction.Enabled := (FServer = nil) or (FServer.Active = False);
+end;
+
+procedure TMainForm.StopServerActionExecute(Sender: TObject);
+begin
+  FServer.Active := False;
+  FreeAndNil(FServer);
+  FreeAndNil(FEngine);
+end;
+
+procedure TMainForm.StopServerActionUpdate(Sender: TObject);
+begin
+  StopServerAction.Enabled := Assigned(FServer) and (FServer.Active = True);
+end;
+
+initialization
+  ReportMemoryLeaksOnShutdown := True;
+
+end.
---
+++ Demos/DelphiRazor/Server.Resources.pas
@@ -0,0 +1,141 @@
+(*
+  Copyright 2016, MARS-Curiosity library
+
+  Home: https://github.com/andrea-magni/MARS
+*)
+unit Server.Resources;
+
+interface
+
+uses
+  SysUtils, Classes, Diagnostics
+
+  , MARS.Core.Attributes
+  , MARS.Core.MediaType
+  , MARS.Core.Response
+
+  , Web.HttpApp
+  , MARS.WebServer.Resources
+  , MARS.Core.URL
+
+  , MARS.Core.Classes
+  , MARS.Core.Engine
+  , MARS.Core.Application
+
+
+  , RlxRazor
+;
+
+type
+  [Path('razor'), Produces(TMediaType.TEXT_HTML)]
+  TRazorResource = class
+  private
+    FRazorEngine: TRlxRazorEngine;
+    FRazorProcessor: TRlxRazorProcessor;
+    function GetCurrentSecond: Integer;
+    function GetCurrentSecondEven: Boolean;
+  protected
+    [Context] URL: TMARSURL;
+    function GetTemplatesFolder: string;
+    function GetFilesFolder: string;
+    function GetHelloText: string;
+  public
+    constructor Create; virtual;
+    destructor Destroy; override;
+
+    [GET, Path('/{*}')]
+    function HelloWorld():string;
+
+    property HelloText: string read GetHelloText;
+    property CurrentSecond: Integer read GetCurrentSecond;
+    property CurrentSecondEven: Boolean read GetCurrentSecondEven;
+  end;
+
+implementation
+
+uses
+    IOUtils, DateUtils
+  , MARS.Core.Registry
+  , MARS.Core.Exceptions
+  ;
+
+{ TRazorResource }
+
+constructor TRazorResource.Create;
+begin
+  inherited Create;
+  FRazorEngine := TRlxRazorEngine.Create(nil);
+  FRazorProcessor := TRlxRazorProcessor.Create(nil);
+  try
+    FRazorProcessor.RazorEngine := FRazorEngine;
+    FRazorEngine.TemplatesFolder := GetTemplatesFolder;
+    FRazorEngine.FilesFolder := GetFilesFolder;
+
+    FRazorEngine.AddToDictionary('resource', Self, False);
+  except
+    FRazorProcessor.Free;
+    raise;
+  end;
+end;
+
+destructor TRazorResource.Destroy;
+begin
+  FRazorProcessor.Free;
+  inherited;
+end;
+
+function TRazorResource.GetCurrentSecond: Integer;
+begin
+  Result := SecondOfTheDay(Now);
+end;
+
+function TRazorResource.GetCurrentSecondEven: Boolean;
+begin
+  Result := (GetCurrentSecond mod 2) = 0;
+end;
+
+function TRazorResource.GetFilesFolder: string;
+begin
+  Result := IncludeTrailingPathDelimiter(
+    TPath.Combine(ExtractFilePath(ParamStr(0)), 'files')
+  );
+end;
+
+function TRazorResource.GetTemplatesFolder: string;
+begin
+  Result := IncludeTrailingPathDelimiter(
+    TPath.Combine(ExtractFilePath(ParamStr(0)), 'templates')
+  );
+end;
+
+function TRazorResource.GetHelloText: string;
+begin
+  Result := 'Hello, current time is: ' + TimeToStr(Now);
+end;
+
+function TRazorResource.HelloWorld(): string;
+var
+  LReader: TStreamReader;
+  LFileName: string;
+begin
+  LFileName := TPath.Combine(GetFilesFolder, URL.PathTokens[Length(URL.PathTokens)-1]);
+  if not FileExists(LFileName) then
+    raise EMARSHttpException.Create(Format('File [%s] not found', [LFileName]), 404);
+
+  LReader := TStreamReader.Create(LFileName);
+  try
+    Result := FRazorProcessor.DoBlock(LReader.ReadToEnd);
+  finally
+    LReader.Free;
+  end;
+end;
+
+initialization
+  TMARSResourceRegistry.Instance.RegisterResource<TRazorResource>(
+    function: TObject
+    begin
+      Result := TRazorResource.Create;
+    end
+  );
+
+end.
