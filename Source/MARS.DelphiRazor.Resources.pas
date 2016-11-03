e5f24d708d879a948b7f1e2950bf7cea54fba377
Andrea Magni <andrea.magni@gmail.com>
2016-11-03 16:57:39 +0100

    * proper ContentType guessing (and override mechanism) * attribute-based override for parameters * support for ErrorPage * refactoring

--- Source/MARS.DelphiRazor.Resources.pas
+++ Source/MARS.DelphiRazor.Resources.pas
@@ -24,6 +24,23 @@ uses
   , RlxRazor
   ;
 type
+  RazorAttribute = class(MARSAttribute);
+
+
+  RazorSingleValueAttribute = class(RazorAttribute)
+  private
+    FValue: string;
+  public
+    constructor Create(AValue: string); virtual;
+    property Value: string read FValue;
+  end;
+
+  RazorHomePageAttribute = class(RazorSingleValueAttribute);
+  RazorErrorPageAttribute = class(RazorSingleValueAttribute);
+  RazorFilesFolderAttribute = class(RazorSingleValueAttribute);
+  RazorTemplatesFolderAttribute = class(RazorSingleValueAttribute);
+
+
   TContextEntry=record
     Name: string;
     Instance: TObject;
@@ -39,12 +56,23 @@ type
   protected
     [Context] URL: TMARSURL;
     [Context] Token: TMARSToken;
-    [Context] ARequest: TWebRequest;
+    [Context] Request: TWebRequest;
+    [Context] Response: TWebResponse;
 
-    function GetTemplatesFolder: string; virtual;
+    function GetRazorAttributeValue<T: RazorSingleValueAttribute>(const AInstance: TObject; const ADefault: string = ''): string;
+    function ContentTypeFromFileExt(const AFileExt: string): string; virtual;
+
+    function GetHomePage: string; virtual;
+    function GetErrorPage: string; virtual;
     function GetFilesFolder: string; virtual;
+    function GetTemplatesFolder: string; virtual;
+
     procedure OnObjectForPathHandler(Sender: TObject; ExecData: TRazorExecData); virtual;
+    procedure OnPageErrorHandler(Sender: TObject; pageInfo: TPageInfo); virtual;
+
     function DoProvideContext(const APathInfo, APathParam: string): TArray<TContextEntry>; virtual;
+    procedure DoError(APageInfo: TPageInfo); virtual;
+    procedure SetResponseContentType(const APathInfo, APathParam, AInputFileName: string); virtual;
 
     property RazorEngine: TRlxRazorEngine read FRazorEngine;
   public
@@ -64,9 +92,34 @@ uses
   , MARS.Core.Registry
   , MARS.Core.Exceptions
   , MARS.Core.Utils
+  , MARS.Rtti.Utils
 ;
 
 { TRazorResource }
+
+function TRazorResource.ContentTypeFromFileExt(const AFileExt: string): string;
+begin
+  if SameText(AFileExt, '.htm') or  SameText(AFileExt, '.html') then
+    Result := 'text/html'
+  else if SameText(AFileExt, '.js') then
+    Result := 'application/javascript'
+  else if SameText(AFileExt, '.json') then
+    Result := 'application/json'
+  else if SameText(AFileExt, '.xml') then
+    Result := 'application/xml'
+  else if SameText(AFileExt, '.csv') then
+    Result := 'text/csv'
+  else if SameText(AFileExt, '.css') then
+    Result := 'text/css'
+  else if SameText(AFileExt, '.jpg') or SameText(AFileExt, '.jpeg') then
+    Result := 'image/jpeg'
+  else if SameText(AFileExt, '.png') then
+    Result := 'image/png'
+  else if SameText(AFileExt, '.gif') then
+    Result := 'image/gif'
+  else
+    Result := '';
+end;
 
 constructor TRazorResource.Create;
 begin
@@ -75,10 +128,11 @@ begin
   try
     FRazorEngine.TemplatesFolder := GetTemplatesFolder;
     FRazorEngine.FilesFolder := GetFilesFolder;
-    FRazorEngine.HomePage := 'index';
-    FRazorEngine.OnObjectForPath := OnObjectForPathHandler;
+    FRazorEngine.HomePage := GetHomePage;
+    FRazorEngine.ErrorPage := GetErrorPage;
 
-    FRazorEngine.AddToDictionary('resource', Self, False);
+    FRazorEngine.OnObjectForPath := OnObjectForPathHandler;
+    FRazorEngine.OnPageError := OnPageErrorHandler;
   except
     FRazorEngine.Free;
     raise;
@@ -91,10 +145,14 @@ begin
   inherited;
 end;
 
+procedure TRazorResource.DoError(APageInfo: TPageInfo);
+begin
+end;
+
 function TRazorResource.DoProvideContext(const APathInfo,
   APathParam: string): TArray<TContextEntry>;
 begin
-  Result := [];
+  Result := [TContextEntry.Create('resource', Self, False)];
 end;
 
 function TRazorResource.GetDocument: string;
@@ -104,7 +162,7 @@ begin
   RazorEngine.BasePath := URL.BasePath + URL.Resource;
 
   Result := RazorEngine.ProcessRequest(
-      ARequest
+      Request
     , LFound
     , Token.IsVerified
     , Token.Claims['LanguageID'].AsInteger
@@ -113,17 +171,46 @@ begin
   );
 end;
 
+function TRazorResource.GetErrorPage: string;
+begin
+  Result := GetRazorAttributeValue<RazorErrorPageAttribute>(Self, 'error.html');
+end;
+
 function TRazorResource.GetFilesFolder: string;
 begin
-  Result := IncludeTrailingPathDelimiter(
-    TPath.Combine(ExtractFilePath(ParamStr(0)), 'files')
+  Result := GetRazorAttributeValue<RazorFilesFolderAttribute>(Self,
+    IncludeTrailingPathDelimiter(
+      TPath.Combine(ExtractFilePath(ParamStr(0)), 'files')
+    )
   );
+end;
+
+function TRazorResource.GetHomePage: string;
+begin
+  Result := GetRazorAttributeValue<RazorHomePageAttribute>(Self, 'index');
+end;
+
+function TRazorResource.GetRazorAttributeValue<T>(const AInstance: TObject;
+  const ADefault: string): string;
+var
+  LValue: string;
+begin
+  LValue := ADefault;
+  TRttiHelper.IfHasAttribute<T>(Self
+    , procedure (AAttrib: T)
+      begin
+        LValue := AAttrib.Value;
+      end
+  );
+  Result := LValue;
 end;
 
 function TRazorResource.GetTemplatesFolder: string;
 begin
-  Result := IncludeTrailingPathDelimiter(
-    TPath.Combine(ExtractFilePath(ParamStr(0)), 'templates')
+  Result := GetRazorAttributeValue<RazorTemplatesFolderAttribute>(Self,
+    IncludeTrailingPathDelimiter(
+      TPath.Combine(ExtractFilePath(ParamStr(0)), 'templates')
+    )
   );
 end;
 
@@ -137,6 +224,20 @@ begin
 
   for LEntry in LContext do
     ExecData.razorProc.AddToDictionary(LEntry.Name, LEntry.Instance, LEntry.Owned);
+
+  SetResponseContentType(ExecData.PathInfo, ExecData.PathParam, ExecData.razorProc.InputFilename);
+end;
+
+procedure TRazorResource.OnPageErrorHandler(Sender: TObject;
+  pageInfo: TPageInfo);
+begin
+  DoError(pageInfo);
+end;
+
+procedure TRazorResource.SetResponseContentType(const APathInfo, APathParam,
+  AInputFileName: string);
+begin
+  Response.ContentType := ContentTypeFromFileExt(ExtractFileExt(AInputFilename));
 end;
 
 { TContextEntry }
@@ -156,6 +257,14 @@ begin
   Instance := nil;
   InitFunction := AInitFunction;
   Owned := true;
+end;
+
+{ RazorSingleValueAttribute }
+
+constructor RazorSingleValueAttribute.Create(AValue: string);
+begin
+  inherited Create;
+  FValue := AValue;
 end;
 
 end.
