36880650af011909148d61291acbd0705c751bdf
Andrea Magni <andrea.magni@gmail.com>
2017-01-31 16:06:00 +0100

    * added TStreamReader (to read de-serialize binary data into TStreams instances)

--- Source/MARS.Core.MessageBodyReaders.pas
+++ Source/MARS.Core.MessageBodyReaders.pas
@@ -22,7 +22,6 @@ type
   [Consumes(TMediaType.APPLICATION_JSON)]
   TJSONValueReader = class(TInterfacedObject, IMessageBodyReader)
   public
-    function AsObject: TObject;
 {$ifdef Delphi10Berlin_UP}
     function ReadFrom(const AInputData: TBytes;
       const AAttributes: TAttributeArray;
@@ -34,6 +33,21 @@ type
 {$endif}
   end;
 
+  [Consumes(TMediaType.APPLICATION_OCTET_STREAM), Consumes(TMediaType.WILDCARD)]
+  TStreamReader = class(TInterfacedObject, IMessageBodyReader)
+  public
+{$ifdef Delphi10Berlin_UP}
+    function ReadFrom(const AInputData: TBytes;
+      const AAttributes: TAttributeArray;
+      AMediaType: TMediaType; ARequestHeaders: TStrings): TValue; virtual;
+{$else}
+    function ReadFrom(const AInputData: AnsiString;
+      const AAttributes: TAttributeArray;
+      AMediaType: TMediaType; ARequestHeaders: TStrings): TValue; virtual;
+{$endif}
+  end;
+
+
 implementation
 
 uses
@@ -44,10 +58,6 @@ uses
 
 { TJSONValueReader }
 
-function TJSONValueReader.AsObject: TObject;
-begin
-  Result := Self;
-end;
 
 {$ifdef Delphi10Berlin_UP}
 function TJSONValueReader.ReadFrom(const AInputData: TBytes;
@@ -77,9 +87,46 @@ begin
 end;
 {$endif}
 
+{ TStreamReader }
+
+{$ifdef Delphi10Berlin_UP}
+function TStreamReader.ReadFrom(const AInputData: TBytes;
+  const AAttributes: TAttributeArray;
+  AMediaType: TMediaType; ARequestHeaders: TStrings): TValue;
+var
+  LStream: TBytesStream;
+begin
+  LStream := TBytesStream.Create(AInputData);
+  try
+    LStream.Position := 0;
+    Result := LStream;
+  except
+    LStream.Free;
+    raise;
+  end;
+end;
+{$else}
+function TStreamReader.ReadFrom(const AInputData: AnsiString;
+  const AAttributes: TAttributeArray;
+  AMediaType: TMediaType; ARequestHeaders: TStrings): TValue;
+var
+  LStream: TStringStream;
+begin
+  LStream := TStringStream.Create(AInputData);
+  try
+    LStream.Position := 0;
+    Result := LStream;
+  except
+    LStream.Free;
+    raise;
+  end;
+end;
+{$endif}
+
 procedure RegisterReaders;
 begin
   TMARSMessageBodyReaderRegistry.Instance.RegisterReader<TJSONValue>(TJSONValueReader);
+  TMARSMessageBodyReaderRegistry.Instance.RegisterReader<TStream>(TStreamReader);
 end;
 
 initialization
