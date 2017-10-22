a4a6e684327e89c84384a5ae876cd7a98e41a1e1
Andrea Magni <andrea.magni@gmail.com>
2017-10-22 22:33:44 +0200

    * EKON21 clean project

---
+++ Demos/EKON21/Server.Resources.pas
@@ -0,0 +1,54 @@
+(*
+  Copyright 2016, MARS-Curiosity - REST Library
+
+  Home: https://github.com/andrea-magni/MARS
+*)
+unit Server.Resources;
+
+interface
+
+uses
+  SysUtils, Classes
+
+  , MARS.Core.Attributes
+  , MARS.Core.MediaType
+  , MARS.Core.JSON
+  , MARS.Core.Response
+  , MARS.Core.URL
+
+  , MARS.Core.Token.Resource
+  ;
+
+type
+  [Path('helloworld')]
+  THelloWorldResource = class
+  protected
+  public
+    [GET, Produces(TMediaType.TEXT_PLAIN)]
+    function SayHelloWorld: string;
+  end;
+
+  [Path('token')]
+  TTokenResource = class(TMARSTokenResource)
+  end;
+
+implementation
+
+uses
+    MARS.Core.Registry
+{$IFDEF MSWINDOWS}
+  , CodeSiteLogging
+{$ENDIF}
+;
+
+{ THelloWorldResource }
+
+function THelloWorldResource.SayHelloWorld: string;
+begin
+  Result := 'Hello World!';
+end;
+
+initialization
+  TMARSResourceRegistry.Instance.RegisterResource<THelloWorldResource>;
+  TMARSResourceRegistry.Instance.RegisterResource<TTokenResource>;
+end.
