e927c546dfa97bb4fbcc2d18232674b058aada77
Andrea Magni <andrea.magni@gmail.com>
2019-05-23 19:07:02 +0200

    Fixed compilation issue. Master branch still has no clue about MARS.RequestAndResponse.Interfaces, reverting some changes from the previous commit.

--- Demos/Mustache/Server.Forms.Main.pas
+++ Demos/Mustache/Server.Forms.Main.pas
@@ -44,7 +44,7 @@ implementation
 
 uses
   StrUtils, Web.HttpApp
-  , MARS.Core.URL, MARS.Core.Engine, MARS.Core.RequestAndResponse.Interfaces
+  , MARS.Core.URL, MARS.Core.Engine
   , Server.Ignition;
 
 procedure TMainForm.FormClose(Sender: TObject; var Action: TCloseAction);
@@ -59,7 +59,7 @@ begin
   // skip favicon requests (browser)
   TServerEngine.Default.BeforeHandleRequest :=
     function (const AEngine: TMARSEngine;
-      const AURL: TMARSURL; const ARequest: IMARSRequest; const AResponse: IMARSResponse;
+      const AURL: TMARSURL; const ARequest: TWebRequest; const AResponse: TWebResponse;
       var Handled: Boolean
     ): Boolean
     begin
@@ -116,3 +116,4 @@ begin
 end;
 
 end.
+
