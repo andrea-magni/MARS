bc2487cc7dd700ac3252dafd15efe82c612555b9
Nando Dessena <nando@dessena.it>
2020-10-07 14:03:09 +0200

    * TMARSFDResource: fix AV when deleting datasets located in different units.

--- Source/MARS.Client.FireDAC.pas
+++ Source/MARS.Client.FireDAC.pas
@@ -19,6 +19,8 @@ uses
 ;
 
 type
+  TMARSFDResourceDatasets = class;
+
   TMARSFDResourceDatasetsItem = class(TCollectionItem)
   private
     FDataSet: TFDMemTable;
@@ -26,6 +28,7 @@ type
     FSendDelta: Boolean;
     FSynchronize: Boolean;
     procedure SetDataSet(const Value: TFDMemTable);
+    function Collection: TMARSFDResourceDatasets;
   protected
     procedure AssignTo(Dest: TPersistent); override;
     function GetDisplayName: string; override;
@@ -40,6 +43,7 @@ type
 
   TMARSFDResourceDatasets = class(TCollection)
   private
+    FOwnerComponent: TComponent;
     function GetItem(Index: Integer): TMARSFDResourceDatasetsItem;
   public
     function Add: TMARSFDResourceDatasetsItem;
@@ -304,6 +308,7 @@ constructor TMARSFDResource.Create(AOwner: TComponent);
 begin
   inherited;
   FResourceDataSets := TMARSFDResourceDatasets.Create(TMARSFDResourceDatasetsItem);
+  FResourceDataSets.FOwnerComponent := Self;
   SpecificAccept := TMediaType.APPLICATION_JSON_FireDAC + ',' + TMediaType.APPLICATION_JSON;
   SpecificContentType := TMediaType.APPLICATION_JSON_FireDAC;
 end;
@@ -409,6 +414,11 @@ begin
   LDest.Synchronize := Synchronize;
 end;
 
+function TMARSFDResourceDatasetsItem.Collection: TMARSFDResourceDatasets;
+begin
+  Result := inherited Collection as TMARSFDResourceDatasets;
+end;
+
 constructor TMARSFDResourceDatasetsItem.Create(Collection: TCollection);
 begin
   inherited;
@@ -427,9 +437,12 @@ procedure TMARSFDResourceDatasetsItem.SetDataSet(const Value: TFDMemTable);
 begin
   if FDataSet <> Value then
   begin
+    if Assigned(FDataSet) then
+      FDataSet.RemoveFreeNotification(Collection.FOwnerComponent);
     FDataSet := Value;
     if Assigned(FDataSet) then
     begin
+      FDataSet.FreeNotification(Collection.FOwnerComponent);
       if SendDelta then
         FDataSet.CachedUpdates := True;
       FDataSet.ActiveStoredUsage := [];
