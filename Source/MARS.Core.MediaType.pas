39eec513529fabad19afb98a91cc58b3c18f5173
Andrea Magni <andrea.magni@gmail.com>
2021-01-15 13:35:39 +0100

    Improved Accept parsing

--- Source/MARS.Core.MediaType.pas
+++ Source/MARS.Core.MediaType.pas
@@ -339,17 +339,24 @@ var
   LMediaStr: string;
   LMediaType: TMediaType;
   LIndex, LLength: Integer;
+  LAdded: Tarray<string>;
 begin
   Clear;
   LMediaArray := TArray<string>(SplitString(FAccept, DELIM_ACCEPT));
   LLength := Length(LMediaArray);
 
+  LAdded := [];
   for LIndex := Low(LMediaArray) to High(LMediaArray) do
   begin
     LMediaStr := LMediaArray[LIndex];
-    LMediaType := TMediaType.Create(LMediaStr);
-    LMediaType.PFactor := LLength - LIndex;
-    FMediaTypeList.Add(LMediaType);
+
+    if IndexText(LMediaStr, LAdded) = -1 then
+    begin
+      LMediaType := TMediaType.Create(LMediaStr);
+      LMediaType.PFactor := LLength - LIndex;
+      FMediaTypeList.Add(LMediaType);
+    end
+    else LLength := LLength - 1;
   end;
 
   if FMediaTypeList.Count = 0 then
